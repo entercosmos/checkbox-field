840bdef2f5331b8a018be57ec9638ced2cc5f44d
keyflight <olivier@andev.nl>
2019-04-28 12:05:37 +0700

    Update demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 import {css, injectGlobal} from 'emotion'
 import CheckboxField from '../../src'
+import {Canvas, Heading, Paragraph, Box} from '@cmds/demo-utils'
 
 injectGlobal`
     * {
@@ -9,6 +10,7 @@ injectGlobal`
     }
     body {
         font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+        margin: 0;
     }
 `
 
@@ -30,52 +32,73 @@ class Demo extends Component {
     }
 
     render() {
-        return <div>
-            <h1>CheckboxField Demo</h1>
-            <p>
-                Used for making a choice between yes and no.
-            </p>
-            <Context contextId={'recordDetail'} roleId={'editor'}/>
-            <CheckboxField
-                id={'fld1'}
-                contextId={'recordDetail'}
-                roleId={'editor'}
-                checked={this.state.checked}
-                onChange={({id, checked}) => {
-                    this.setState({checked})
-                }}
-            />
-            <Context contextId={'recordDetail'} roleId={'readOnly'}/>
-            <CheckboxField
-                id={'fld1'}
-                contextId={'recordDetail'}
-                roleId={'readOnly'}
-                checked={this.state.checked}
-                onChange={({id, checked}) => {
-                    this.setState({checked})
-                }}
-            />
-            <Context contextId={'recordGalleryCard'} roleId={'readOnly'}/>
-            <CheckboxField
-                id={'fld1'}
-                contextId={'recordGalleryCard'}
-                roleId={'readOnly'}
-                checked={this.state.checked}
-                onChange={({id, checked}) => {
-                    this.setState({checked})
-                }}
-            />
-            <Context contextId={'recordListItem'} roleId={'readOnly'}/>
-            <CheckboxField
-                id={'fld1'}
-                contextId={'recordListItem'}
-                roleId={'readOnly'}
-                checked={this.state.checked}
-                onChange={({id, checked}) => {
-                    this.setState({checked})
-                }}
-            />
-        </div>
+        return <Canvas>
+            <Heading>
+                Record Detail Context
+            </Heading>
+            <Paragraph>
+                With editor role
+            </Paragraph>
+            <Box>
+                <CheckboxField
+                    id={'fld1'}
+                    contextId={'recordDetail'}
+                    roleId={'editor'}
+                    checked={this.state.checked}
+                    onChange={({id, checked}) => {
+                        this.setState({checked})
+                    }}
+                />
+            </Box>
+            <Paragraph>
+                With read only role
+            </Paragraph>
+            <Box>
+                <CheckboxField
+                    id={'fld1'}
+                    contextId={'recordDetail'}
+                    roleId={'readOnly'}
+                    checked={this.state.checked}
+                    onChange={({id, checked}) => {
+                        this.setState({checked})
+                    }}
+                />
+            </Box>
+            <Heading>
+                Record Gallery Card Context
+            </Heading>
+            <Paragraph>
+                With read only role
+            </Paragraph>
+            <Box>
+                <CheckboxField
+                    id={'fld1'}
+                    contextId={'recordGalleryCard'}
+                    roleId={'readOnly'}
+                    checked={this.state.checked}
+                    onChange={({id, checked}) => {
+                        this.setState({checked})
+                    }}
+                />
+            </Box>
+            <Heading>
+                Record List Item Context
+            </Heading>
+            <Paragraph>
+                With read only role
+            </Paragraph>
+            <Box>
+                <CheckboxField
+                    id={'fld1'}
+                    contextId={'recordListItem'}
+                    roleId={'readOnly'}
+                    checked={this.state.checked}
+                    onChange={({id, checked}) => {
+                        this.setState({checked})
+                    }}
+                />
+            </Box>
+        </Canvas>
     }
 }
 
